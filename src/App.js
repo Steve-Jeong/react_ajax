b32113e8d4bbdece43e9bbda0b22275a891823a1
Steve Jeong <jst0930@gmail.com>
2020-10-13 21:45:35 +0900

    base

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Nav extends Component { 
+  state = { 
+    list : []
+  }
+
+  componentDidMount() {
+    fetch('list.json')
+      .then(function(result) {
+        console.log('result : ', result)
+        return result.json();
+      })
+      .then(function(json) {
+        console.log('json : ',json)
+        this.setState({list:json})
+      }.bind(this))
+  }
+
+  render() {
+    var listTag = []
+    for(let i=0; i < this.state.list.length; i++) {
+      let li=this.state.list[i]
+      listTag.push(
+        <li key={li.id}>
+          <a href={li.id} onClick={function(id, e) {
+            e.preventDefault();
+            this.props.onClick(id)
+          }.bind(this, li.id)}>
+            {li.title}
+          </a>
+        </li>
+      )
+    }
+    return (
+      <ul>
+        {listTag}
+      </ul>
+    )
+  }
+}
+
+class Article extends Component {
+  render () {
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    )
+  }
+}
+
+class App extends Component {
+  state = {
+    article:[
+      {title:'Welcome', desc:'Hello, React & Ajax'},
+      {title:'HTML', desc:'HTML is ...'},
+      {title:'CSS', desc:'CSS is ...'},
+      {title:'JavaScript', desc:'JavaScript is ...'}
+    ],
+    selected_id : 0
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>WEB</h1>
+        <Nav onClick={function(id) {
+          console.log('id', id)
+          this.setState({selected_id:id})
+          console.log('this.state.selected_id :', this.state.selected_id)
+        }.bind(this)}></Nav>
+        <Article title={this.state.article[this.state.selected_id].title} desc={this.state.article[this.state.selected_id].desc}></Article>
+        {/* <Article title={this.state.selected_id} desc={this.state.selected_id}></Article> */}
+      </div>
+    )
+  }
 }
 
 export default App;
